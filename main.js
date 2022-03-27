306abde6ac790dcd0ee29c90efdba16a2a8b2912
Henry Salazar <hjsmipuc@gmail.com>
2022-03-27 18:01:46 -0500

    programación del codigo para desencriptar el mensaje

--- main.js
+++ main.js
@@ -8,13 +8,83 @@ const inputTexto = document.querySelector('[data-input-texto]');
 const decodificarTexto = () => {
     const value = inputTexto.value;
     const arrayTexto = value.split('');
-    console.log(arrayTexto);
 
+    let contador = 0;
+    let elemento = null;
+    const textoDesencriptado = [];
+
+    arrayTexto.forEach( item => {
+        
+        if (contador === 0) {
+            switch(item){
+                case 'a':
+                    elemento = item;
+                    contador = 1;
+                    break;
+                case 'e':
+                    elemento = item;
+                    contador = 4;
+                    break;
+                case 'i':
+                    elemento = item;
+                    contador = 3;
+                    break;
+                case 'o': 
+                    elemento = item;
+                    contador = 3;
+                    break;
+                case 'u':
+                    elemento = item;
+                    contador = 3;
+                    break;
+                default:
+                    elemento = item;
+            }
+            textoDesencriptado.push(elemento);
+        }
+        else {
+            contador--;
+        }
+
+    });
+  
+    const mostrarTexto = document.querySelector('[data-display]');
+    mostrarTexto.value = textoDesencriptado.join('');
+
+    const eliminarMensaje = document.querySelector('#eliminar-mensaje');
+    eliminarMensaje.classList.add('eliminar-mensaje');
+
+    const visualizarMensaje = document.querySelector('#visualizar-mensaje');
+    visualizarMensaje.classList.add('visualizar-mensaje')
+
+    inputTexto.value = '';
 }
 
 
 btnEncriptar.addEventListener('click', codificarTexto);
 btnDesencriptar.addEventListener('click', decodificarTexto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
