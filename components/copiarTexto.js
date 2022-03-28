863b50c6905efb6c5d9da2e25c056e791ca18cc5
Henry Salazar <hjsmipuc@gmail.com>
2022-03-28 17:14:48 -0500

    programación del mensaje de copiado al dar click al boton copiar

--- components/copiarTexto.js
+++ components/copiarTexto.js
@@ -1,8 +1,14 @@
 const copiarTexto = () => {
     const mostrarTexto = document.querySelector('[data-display]');
     const cb = navigator.clipboard;
-    cb.writeText(mostrarTexto.value);
-    mostrarTexto.value = '';
+    cb.writeText(mostrarTexto.value).then(()=> {
+        mostrarTexto.value = '';
+        const mensaje = document.querySelector('[data-mensaje-copiado]');
+        mensaje.classList.add('mensaje__copiado-activo');
+        setTimeout( () => {
+            mensaje.classList.remove('mensaje__copiado-activo');
+        }, 3000);
+    })
 }
 
 export default copiarTexto;
